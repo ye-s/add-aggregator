40cc87ca55174fceb176046ba43db9a5a19b3aeb
Yevgeniy <jenyaslu@gmail.com>
2019-06-18 23:43:37 +0300

    Updates

---
+++ precache-manifest.06d7546e8907c5a846790736730f6a9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c29112827784a210b6c4cc8a059b77c",
+    "url": "/add-aggregator/index.html"
+  },
+  {
+    "revision": "fa1d379414f5fd0a0241",
+    "url": "/add-aggregator/static/css/main.04111cc5.chunk.css"
+  },
+  {
+    "revision": "2532cc153ad425ccaa83",
+    "url": "/add-aggregator/static/js/2.9fb2c524.chunk.js"
+  },
+  {
+    "revision": "fa1d379414f5fd0a0241",
+    "url": "/add-aggregator/static/js/main.f4d327fb.chunk.js"
+  },
+  {
+    "revision": "41913b15b8098d8b9838",
+    "url": "/add-aggregator/static/js/runtime~main.1e91c8e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/add-aggregator/static/media/logo.5d5d9eef.svg"
+  }
+]);
